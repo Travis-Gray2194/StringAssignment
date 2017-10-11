323eb25ad5f7451791817994fc23cf4ee16af31e
Travis-Gray2194 <travisogray1@gmail.com>
2017-10-11 18:01:55 -0400

    Initial commit

---
+++ src/StringAssignment.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+/**
+ * Created by ${TravisGray} on 10/11/2017.
+ */
+
+//Write a Java program that assigns your name to a variable and prints the variable to the console.
+    
+public class StringAssignment {
+    public static void main (String[] args){
+
+        Scanner keyboard = new Scanner(System.in);
+        String name = "Travis Gray";
+        System.out.println(name);
+    }
+}
